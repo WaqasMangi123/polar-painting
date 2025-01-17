d313005af22eaaedda9efddff9cdfbfaa1e706cb
kashiekzmi <kashifkzmi5@gmail.com>
2025-01-17 23:33:23 +0500

    cors reemoving

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,9 @@ const app = express();
 
 // CORS options configuration
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
 };
 
 app.use(express.json());
